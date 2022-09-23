fe0b922055bc81016a97f472fb088084158561e3
CFirnkoess <Truly@LAPTOP-73S7HVL7>
2022-09-23 16:58:21 -0700

    First Commit

---
+++ src/main/java/com/gcu/MilestoneCst339Application.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestoneCst339Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilestoneCst339Application.class, args);
+	}
+
+}
