03200c2ec8ef32630b325a470e9ac1f0d9daec74
CFirnkoess <Truly@LAPTOP-73S7HVL7>
2022-09-28 11:40:31 -0700

    Hello world practice

---
+++ src/main/java/com/gcu/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.gcu;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
